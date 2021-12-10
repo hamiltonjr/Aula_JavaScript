1bb9ea8effe53399ca52736f3f867f958336884a
Hamilton Gonçalves junior <hamiltonjr2010@gmail.com>
2021-12-09 23:17:34 -0300

    Confirmação inicial (já com algum código).

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// código básico 
+// popup
+alert('Olá!');
+
+// variáveis
+var nome = 'Hamilton';
+alert('Olá ' + nome);
+console.log('Olá' + nome);
+
+var idade = prompt('Idade');
+alert('Idade: ' + idade);
+console.log('Idade: ' + idade);
+
+// declaração de variável
+var counter;
+
+// loop while
+counter = 0;
+alert('Contando até 10 com while...');
+console.log('Contando até 10 com while...');
+while (counter < 10) {
+    counter++; 
+    console.log(counter);
+}
+
+// loop for
+alert('Contando até 10 com for...');
+console.log('Contando até 10 com for...');
+for (counter = 0; counter < 10; counter++) {
+    console.log(counter + 1);
+}
